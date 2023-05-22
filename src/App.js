fc332f157d116afd83e63554b1ebb47ac6e6dfae
ShigatsuEl <shigatsu970704@gmail.com>
2023-05-22 20:40:03 +0900

    feat: 플레이어 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import { useCallback, useEffect } from "react";
 import "./App.css";
 
+const playerOpts = {
+    videoId: "",
+};
+
 function App() {
+    let internalPlayer = null;
+    let destroyPlayerPromise = undefined;
+
+    const createPlayer = useCallback(() => {
+        if (typeof document === "undefined") return;
+        if (destroyPlayerPromise) {
+            // We need to first await the existing player to be destroyed before
+            // we can re-create it.
+            destroyPlayerPromise.then(createPlayer);
+            return;
+        }
+    }, [destroyPlayerPromise]);
+
+    const destroyPlayer = () => {
+        if (internalPlayer) {
+            destroyPlayerPromise = internalPlayer
+                .destroy()
+                .then(() => (destroyPlayerPromise = undefined));
+            return destroyPlayerPromise;
+        }
+        return Promise.resolve();
+    };
+
+    const resetPlayer = () => {
+        destroyPlayer().then(createPlayer);
+    };
+
+    useEffect(() => {
+        createPlayer();
+    }, [createPlayer]);
+
     return <div className="App"></div>;
 }
 
